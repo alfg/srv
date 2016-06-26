e07ce7cc31a7a8e2a184982b05613d03abf1c621
Alf <alf.g.jr@gmail.com>
2016-06-25 20:13:55 -0700

    Added logging/winston middleware.

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 export default function test(app) {
-    console.log('test');
     app.get('/test', (req, res) => {
+        app.logger.info('log something else');
         res.send('test OK')
     })
 }

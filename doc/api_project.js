0afb3663589a59e17da5f9a1a7428d2b685e4099
Alf <alf.g.jr@gmail.com>
2016-06-26 22:58:27 -0700

    Add apidoc generation.

---
+++ doc/api_project.js
@@ -0,0 +1,13 @@
+define({
+  "name": "srv",
+  "version": "0.1.0",
+  "description": "A modern, opinionated and simple microservices stack built on Express.",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-06-27T05:57:59.324Z",
+    "url": "http://apidocjs.com",
+    "version": "0.16.1"
+  }
+});
--- test/test.js
+++ test/test.js
@@ -1,3 +1,13 @@
+/**
+ * @api {get} /user/:id Request User information
+ * @apiName GetUser
+ * @apiGroup User
+ *
+ * @apiParam {Number} id Users unique ID.
+ *
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ */
 export default function test(app) {
     app.get('/test', (req, res) => {
         app.logger.info('log something else');

110996682cebccb814c77f1cac9e9d7e3b993047
Alf <alf.g.jr@gmail.com>
2016-07-13 01:09:25 -0700

    Fixing tests.

--- test/api.js
+++ test/api.js
@@ -1,45 +1,33 @@
 import test from 'ava';
 import srv from '../build/lib/core.js';
-import request from 'request-promise';
-
-
-const listen = (fn) => {
-  const server = srv(fn);
-  const port = 0;
-  const host = '127.0.0.1';
-
-  return new Promise((resolve, reject) => {
-    const listener = server.listen(port, host, err => {
-      if (err) return reject(err);
-      const port = listener.address().port;
-      resolve(`http://${host}:${port}`);
-    });
-  });
-}
-
+import request from 'supertest-as-promised';
+import express from 'express';
 
 test('res.send(200 <String>)', async t => {
-  const fn = async (app) => {
+  function fn(app) {
     app.get('/', (req, res) => {
       res.send('hello');
     });
+    return app;
   }
 
-  const url = await listen(fn);
-  const res = await request(url);
+  const test = srv(fn);
+  const res = await request(test)
+    .get('/').send();
 
-  t.deepEqual(res, 'hello');
+  t.deepEqual(res.text, 'hello');
 });
 
 test('res.json(200 <Object>)', async t => {
-  const fn = async (app) => {
-    app.get('/hello', (req, res) => {
+  function fn(app) {
+    app.get('/', (req, res) => {
       res.json({ hello: 'world' });
     });
+    return app;
   }
 
-  const url = await listen(fn);
-  const res = await request(url + '/hello', { json: true});
-
-  t.deepEqual(res, { hello: 'world' });
+  const test = srv(fn);
+  const res = await request(test)
+    .get('/').send();
+  t.deepEqual(res.body, { hello: 'world' });
 });
